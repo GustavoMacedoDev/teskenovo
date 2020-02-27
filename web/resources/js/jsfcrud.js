c022c4af0ce627e8e91a1691942525a4a1c2b72c
GustavoMacedoDev <39140951+GustavoMacedoDev@users.noreply.github.com>
2020-02-27 02:48:07 -0300

    27-02

--- web/resources/js/jsfcrud.js
+++ web/resources/js/jsfcrud.js
@@ -1,3 +1,24 @@
+function somar() {
+    
+    console.log('opa');
+    
+    var teste = null;
+    teste = document.getElementsByClassName('valor');
+
+    console.log(teste);
+    console.log('eae');
+
+    var total = 0;
+    for (let i = 0; i < teste.length; i++) {
+        let valor = parseFloat(teste[i].textContent);
+        total = total + valor;
+    }
+    
+    console.log(total);
+    
+    document.getElementById('resulta:resultado').innerHTML = total;
+};
+
 function handleSubmit(args, dialog) {
     var jqDialog = jQuery('#' + dialog);
     if (args.validationFailed) {
@@ -7,16 +28,3 @@ function handleSubmit(args, dialog) {
     }
 }
 
-function soma() {
-    let tdsValores = document.querySelectorAll('.valor');
-    console.log(tdsValores);
-
-    let total = 0;
-    for (let i = 0; i < tdsValores.length; i++) {
-        let valor = parseFloat(tdsValores[i].textContent);
-        total = total + valor;
-    }
-    console.log(total);
-    document.getElementById('resultado').innerHTML = total;
-}
-;
